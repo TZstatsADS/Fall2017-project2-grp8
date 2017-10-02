c03b7cd6e02f011d84f99cccdec98f0493482926
Peter Li <pli@ny-pli2.ondeck.local>
2017-10-02 15:47:05 -0400

    adding sample of shiny gui

--- app/ui.r
+++ app/ui.r
@@ -1,42 +1,71 @@
-library(shiny)
-library(leaflet)
+packages.used <- 
+  c("ggplot2",
+    "plyr",
+    "reshape2",
+    "reshape",
+    "shiny",
+    "dplyr",
+    "lubridate",
+    "zoo"
+  )
 
-# Define UI for application that draws a histogram
+# check packages that need to be installed.
+packages.needed=setdiff(packages.used, 
+                        intersect(installed.packages()[,1], 
+                                  packages.used))
+# install additional packages
+if(length(packages.needed)>0){
+  install.packages(packages.needed, dependencies = TRUE)
+}
+
+#load the packages
+library(ggplot2)
+library(zoo)
+library(lubridate)
+library(dplyr)
+
+#source
+source("../lib/plot_functions.R")
+source("../lib/filter_data_functions.R")
+
+#load data
+raw_data = read.csv("../output/flight_data.csv")
+
+dest_airport=c('All',as.character(sort(unique(raw_data$dest))))
+orig_airport=c('All',as.character(sort(unique(raw_data$orig))))
+
+#
+# ui.R
+
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("2009 Manhattan Housing Sales"),
+  titlePanel("Satisfication Probability of Carriers"),
   
   # Sidebar with a selector input for neighborhood
   sidebarLayout(
     sidebarPanel(
-      selectInput("nbhd", label = h5("Choose a Manhattan Neighborhood"), 
-                         choices = list("all neighborhoods"=0,
-                                        "Central Harlem"=1, 
-                                        "Chelsea and Clinton"=2,
-                                        "East Harlem"=3, 
-                                        "Gramercy Park and Murray Hill"=4,
-                                        "Greenwich Village and Soho"=5, 
-                                        "Lower Manhattan"=6,
-                                        "Lower East Side"=7, 
-                                        "Upper East Side"=8, 
-                                        "Upper West Side"=9,
-                                        "Inwood and Washington Heights"=10), 
-                         selected = 0)
-      #sliderInput("p.range", label=h3("Price Range (in thousands of dollars)"),
-      #            min = 0, max = 20000, value = c(200, 10000))
+      selectInput(inputId = "destination",
+                           label  = "Select the Destination",
+                           choices = dest_airport,
+                           selected ='All'),
+               selectInput(inputId = "origin",
+                           label  = "Select the Origin",
+                           choices = orig_airport,
+                           selected ='All'),
+               
+               width = 3
     ),
     # Show two panels
     mainPanel(
-      #h4(textOutput("text")),
       h3(code(textOutput("text1"))),
       tabsetPanel(
-        # Panel 1 has three summary plots of sales. 
-        tabPanel("Sales summary", plotOutput("distPlot")), 
-        # Panel 2 has a map display of sales' distribution
-        tabPanel("Sales map", plotOutput("distPlot1"))),
-      leafletOutput("map", width = "80%", height = "400px")
+        tabPanel(h3("Flight Delay Analysis"),
+                 plotOutput("plt_delay_time"),
+                 plotOutput("plt_delay_flight_distr"),
+                 plotOutput("plt_delay_time_distr"),
+                 value=0)
+        )
     )
  )
 ))
-
