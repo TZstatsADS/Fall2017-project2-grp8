9fde5feeb1b678247fbed58f45eabb8c7bf20fa9
christinahyc <yh2859@columbia.edu>
2017-10-08 17:46:06 -0400

    change file path

--- doc/flight_viz_fashion/RShingMap/global.R
+++ doc/flight_viz_fashion/RShingMap/global.R
@@ -6,12 +6,12 @@ library(leaflet)
 library(geosphere)
 library(sparklyr)
 
-flightData <- read.table(file = "/Users/laohuang/Desktop/ADS/ADSforChristinaH/Fall2017-project2-grp8/data/FlightViz/test.csv",
+flightData <- read.table(file = "../../../data/FlightViz/test.csv",
                          as.is = T, header = T,sep = ",")
 flightData <- flightData[,-5]
 flightData$FL_DATE <- strptime(flightData$FL_DATE, "%Y/%m/%d")
 
-airportLocation <- read.table(file = "/Users/laohuang/Desktop/ADS/ADSforChristinaH/Fall2017-project2-grp8/data/FlightViz/test-address.csv",
+airportLocation <- read.table(file = "../../../data/FlightViz/test-address.csv",
                               as.is = T, header = T,sep = ",")
 #rownames(airportLocation) <- airportLocation[,1]
 airportLocation <- airportLocation[,-3]
