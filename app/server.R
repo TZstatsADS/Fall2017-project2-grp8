96166d175866816f07d8e75bdd4d69dd6c9c674a
Peter Li <Li.Peter94@gmail.com>
2017-10-08 18:14:43 -0400

    moving han app to main app folder

--- app/server.R
+++ app/server.R
@@ -1,40 +1,45 @@
+library(shiny)
+
+temp <-  read.csv("../../output/temp.csv",header=T)
+raw_data <-  read.csv("../../output/flight_data.csv")
+tree_final1 <- read.csv("../../output/treemap1.csv",header=TRUE,as.is=TRUE)
+airport_location <- read.csv("../../output/airportlocations.csv")
 
 
+# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  raw_data = read.csv("../output/flight_data.csv")
   
-  filtered_data = reactive({filter_data(raw_data,origin=input$origin,destination=input$destination)})
+  
+  output$map <- renderLeaflet({
+    
+    flight_path(temp,m=input$month,d=input$destination,o=input$origin)
+  })
+  
+  output$delay_barplot <- renderPlotly({
+    delay_barplot(temp,m=input$month,d=input$destination,o=input$origin)
+  })
+   
+  filtered_data = reactive({filter_data(raw_data,origin=input$origin1,destination=input$destination1)})
   
   output$plt_delay_time = renderPlotly(plot_delay_time(filtered_data=filtered_data(),
-                                                     origin=input$origin, 
-                                                     destination = input$destination))
+                                                       origin=input$origin1, 
+                                                       destination = input$destination1))
   output$plt_delay_flight_distr = renderPlotly(plot_delayed_flight_distribution(filtered_data=filtered_data(),
-                                                                              origin=input$origin, 
-                                                                              destination = input$destination))
+                                                                                origin=input$origin1, 
+                                                                                destination = input$destination1))
   output$plt_delay_time_distr = renderPlotly(plot_delay_time_distribution(filtered_data=filtered_data(),
-                                                                        origin=input$origin, 
-                                                                        destination = input$destination))
-
+                                                                          origin=input$origin1, 
+                                                                          destination = input$destination1))
+  
   filtered_data_delay_reason = reactive({filter_data(raw_data,
                                                      origin=input$origin2,
                                                      destination=input$destination2,
-                                                     month=input$month)})
+                                                     month=input$month2)})
   
   output$plt_delay_reason_distr = renderPlotly(plot_delay_reason_distribution(filtered_data=filtered_data_delay_reason(),
-                                                                            origin=input$origin2,
-                                                                            destination=input$destination2,
-                                                                            month=input$month))
-
-  tree_final1=read.csv("../output/treemap1.csv",header=TRUE,as.is=TRUE)
+                                                                              origin=input$origin2,
+                                                                              destination=input$destination2,
+                                                                              month=input$month2))
   
-  output$treemap<-renderPlot({
-    #selcet a destination, an origin, a month and a satisfied time point
-    tree_select=tree_final1%>%
-      filter(dest==input$destination3,orig==input$origin3,month==input$mon3,sat_time==input$satisfy_time3)
-    if(nrow(tree_select)!=0){
-      tree_select$label<-paste(tree_select$carrier,", ",round(100*tree_select$prec,2),"%",sep="")
-      treemap(tree_select,index='label',vSize="prec",vColor="label",type="categorical", palette=rainbow(7),aspRatio=30/30,drop.unused.levels = FALSE, position.legend="none")
-    }
-  })
-
+ 
 })
--- app/ui.r
+++ app/ui.r
@@ -9,7 +9,8 @@ packages.used <-
     "zoo",
     "treemap",
     "plotly",
-    "shinydashboard"
+    "leaflet",
+    "geosphere"
   )
 
 # check packages that need to be installed.
@@ -30,99 +31,127 @@ library(shiny)
 library(shinydashboard)
 library(treemap)
 library(plotly)
-library(reshape)
 library(reshape2)
+library(leaflet)
+library(geosphere)
 
-#source
-source("../lib/plot_functions.R")
-source("../lib/filter_data_functions.R")
 
-#load data
-raw_data = read.csv("../output/flight_data.csv")
+source("../../lib/plot_functions.R")
+source("../../lib/filter_data_functions.R")
+source("../../lib/flight_path_map.R")
+source("../../lib/delay_percent_barplot.R")
 
+
+raw_data = read.csv("../../output/flight_data.csv")
 dest_airport=c('All',as.character(sort(unique(raw_data$dest))))
 orig_airport=c('All',as.character(sort(unique(raw_data$orig))))
 
-#
-# ui.R
-ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Flight Delay Time Expectations", tabName = "first_app"),
-      menuItem("Flight Delay Reasons", tabName = "second_app"),
-      menuItem("Flight Delay Probability", tabName = "third_app")
-    )
-  ),
-  dashboardBody(
-    tabItems(
-      tabItem(tabName = "first_app",
-              h2("Flight Delay Time Expectations"),
-              fluidPage(
-                box(
-                  selectInput(inputId = "destination",
-                              label  = "Select the Destination",
-                              choices = dest_airport,
-                              selected ='All')),
-                box(
-                  selectInput(inputId = "origin",
-                              label  = "Select the Origin",
-                              choices = orig_airport,
-                              selected ='All')
-                  ),
-              box(plotlyOutput("plt_delay_time")),
-              box(plotlyOutput("plt_delay_flight_distr")),
-              box(plotlyOutput("plt_delay_time_distr"))
-      )),
-      
-      tabItem(tabName = "second_app",
-              h2("Flight Delay Reasons"),
-              
-                box(
-                  selectInput(inputId = "destination2",
-                              label  = "Select the Destination",
-                              choices = dest_airport,
-                              selected ='All'),
-                  selectInput(inputId = "origin2",
-                              label  = "Select the Origin",
-                              choices = orig_airport,
-                              selected ='All'),
-                  selectInput(inputId = "month",
-                                label  = "Select the Month",
-                                choices = c('Jan','Feb','Mar','Apr','May','Jun','Jul',
-                                            'Aug','Sep','Oct','Nov','Dec'),
-                                selected ='Jan')),
-                box(plotlyOutput("plt_delay_reason_distr"))
-              
-            ),
-           
-      tabItem(tabName = "third_app",
-              h2("Flight Delay Probability"),
-              
-                box(
-                  selectInput(inputId = "destination3",
-                           label  = "Select the destination",
-                           choices = dest_airport,
-                           selected ='ATL (Atlanta, GA)'),
-                 selectInput(inputId = "origin3",
-                             label  = "Select the origin",
-                             choices = orig_airport,
-                             selected ='AUS (Austin, TX)'),
-                 sliderInput(inputId = "mon3",
-                             label = "Select the month",
-                             value = 1, min =1, max =12),
-                 sliderInput(inputId = "satisfy_time3",
-                             label = "Select the limit of delay time (hr)",
-                             value = 1,min = 0,max = 5)),
-                box(plotOutput("treemap",width = "100%", height = 600),
-                 absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                               draggable = TRUE, 
-                               top = 600, left = 20, right = "auto", bottom = "auto",
-                               width = 350, height = "auto",
-                               plotOutput("ggplot",width="100%",height="250px")
-               ))
-              
-            )
-    )
-  )
-)
+temp <-  read.csv("../../output/temp.csv",header=T)
+origins <- as.character(sort(unique(temp$orig)))
+destinations <- as.character(sort(unique(temp$dest)))
+
+#Define UI for application that draws a histogram
+shinyUI(navbarPage(theme = "bootstrap.min-copy.css",'Flight Delay',
+        tabPanel("Introduction",
+                 mainPanel(
+                   img(src="world_flight.jpg",height='300',width='600'),
+                   h2('Introduction')
+                   )
+                 # titlePanel(h2("Introduction")),
+                 # mainPanel(tabPanel("Introduction"))
+        ),
+        tabPanel('APP',
+                 sidebarLayout(
+                   sidebarPanel(
+                
+                     selectInput(inputId = "origin",
+                                 label  = "Select the Origin",
+                                 choices = origins,
+                                             selected ='JFK (New York, NY)'),
+                     selectInput(inputId = "destination",
+                                 label  = "Select the Destination",
+                                 choices = c('all',destinations),
+                                 selected ='all'),
+                     selectInput(inputId = "month",
+                                 label  = "Select the Month",
+                                 choices = c('Jan','Feb','Mar','Apr','May','Jun','Jul',
+                                             'Aug','Sep','Oct','Nov','Dec'),
+                                 selected ='Jul'),
+                     radioButtons(inputId = "type",
+                                  label = 'Calculated by:',
+                                  choices = c('Percent of delay flights',
+                                              'Average delay time'),
+                                  selected = 'Percent of delay flights'),
+                     width = 3
+                 ),
+                   
+                   mainPanel(
+                     #plotOutput("delay_barplot",height='200px')
+                          
+                     box(leafletOutput("map"),
+                         width=600),
+                     
+                     box(plotlyOutput("delay_barplot",height='200px'),width=300)
+                     )
+                                   
+                   )
+                 ),
+        
+        tabPanel('Delay Time Expectation',
+                 sidebarLayout(
+                   sidebarPanel(
+                     
+                     selectInput(inputId = "destination1",
+                                 label  = "Select the Destination",
+                                 choices = dest_airport,
+                                 selected ='All'),
+                     selectInput(inputId = "origin1",
+                                 label  = "Select the Origin",
+                                 choices = orig_airport,
+                                 selected ='All'),
+                     width = 3
+                   ),
+                   
+                   mainPanel(
+                     box(plotlyOutput("plt_delay_time"),width=300),
+                     box(plotlyOutput("plt_delay_flight_distr"),width=300),
+                     box(plotlyOutput("plt_delay_time_distr"),width=300)
+                     )
+                )
+        ),
+        
+        tabPanel('Delay Reason Expectation',
+                 sidebarLayout(
+                   sidebarPanel(
+                     
+                     selectInput(inputId = "destination2",
+                                 label  = "Select the Destination",
+                                 choices = dest_airport,
+                                 selected ='All'),
+                     selectInput(inputId = "origin2",
+                                 label  = "Select the Origin",
+                                 choices = orig_airport,
+                                 selected ='All'),
+                     selectInput(inputId = "month2",
+                                 label  = "Select the Month",
+                                 choices = c('Jan','Feb','Mar','Apr','May','Jun','Jul',
+                                             'Aug','Sep','Oct','Nov','Dec'),
+                                 selected ='Jan'),
+                     width = 3
+                   ),
+                   
+                   mainPanel(
+                     box(plotlyOutput("plt_delay_reason_distr"),width=300)
+                     )
+                   )
+                 ),
+        
+        tabPanel('Statistics'),
+        tabPanel('About Us',
+                 includeMarkdown('contact.md'))
+        )
+        )
+
+
+
+
