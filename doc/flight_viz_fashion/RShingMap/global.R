42a89002d1d7110c971c0c633f1b28873876a5ea
christinahyc <yh2859@columbia.edu>
2017-10-09 14:52:45 -0400

    map&timeline

--- doc/flight_viz_fashion/RShingMap/global.R
+++ doc/flight_viz_fashion/RShingMap/global.R
@@ -1,3 +1,23 @@
+packages.used <- 
+  c("ggplot2",
+    "shiny",
+    "dplyr",
+    "leaflet",
+    "geosphere",
+    "sparklyr",
+    "DT",
+    "lubridate"
+  )
+
+# check packages that need to be installed.
+packages.needed=setdiff(packages.used, 
+                        intersect(installed.packages()[,1], 
+                                  packages.used))
+# install additional packages
+if(length(packages.needed)>0){
+  install.packages(packages.needed, dependencies = TRUE)
+}
+
 library(shiny)
 library(dplyr)
 library(ggplot2)
@@ -5,18 +25,46 @@ library(DT)
 library(leaflet)
 library(geosphere)
 library(sparklyr)
+library(lubridate)
 
-<<<<<<< Updated upstream
-flightData <- read.table(file = "../../../data/FlightViz/test.csv",
-=======
-flightData <- read.table(file = "../data/FlightViz/test.csv",
->>>>>>> Stashed changes
+
+
+###======== code ========
+##========= data cleaning part =========
+
+#flightData <- read.table(file = "../../../data/FlightViz/test.csv",
+#                         as.is = T, header = T,sep = ",")
+#flightData <- flightData[,-5]
+#flightData$FL_DATE <- strptime(flightData$FL_DATE, "%Y/%m/%d")
+
+#airportLocation <- read.table(file = "../../../data/FlightViz/test-address.csv",
+#                              as.is = T, header = T,sep = ",")
+#airportLocation <- airportLocation[,-3]
+#colnames(airportLocation) <- c("LON", "LAT")
+
+
+
+
+
+#flightData <- read.table(file = "../../../data/FlightViz/199001.csv",
+#                        as.is = T, header = T,sep = ",")
+#flightData <- flightData[, c(1, 2, 3, 4)]
+#flightData <- flightData%>%
+#  group_by(FL_DATE, ORIGIN, DEST) %>%
+#  summarise(meanDelay=(mean(DEP_DELAY_NEW)))
+#flightData <- na.omit(flightData)
+#write.table(flightData, file = "../../../data/FlightViz/199001.csv",
+#            col.names = T, sep = ",")
+flightData <- read.table(file = "../../../data/FlightViz/199001.csv",
                          as.is = T, header = T,sep = ",")
-flightData <- flightData[,-5]
-flightData$FL_DATE <- strptime(flightData$FL_DATE, "%Y/%m/%d")
+flightData$FL_DATE <- parse_date_time(flightData$FL_DATE, "%Y-%m-%d")
 
-airportLocation <- read.table(file = "../../../data/FlightViz/test-address.csv",
+
+
+airportLocation <- read.table(file = "../../../data/FlightViz/airport_location.csv",
                               as.is = T, header = T,sep = ",")
-#rownames(airportLocation) <- airportLocation[,1]
-airportLocation <- airportLocation[,-3]
+airportLocation <- airportLocation[!duplicated(airportLocation$IATA),]
+rownames(airportLocation) <- airportLocation$IATA
+airportLocation <- airportLocation[,-1]
 colnames(airportLocation) <- c("LON", "LAT")
+airportLocation <- na.omit(airportLocation)
--- doc/flight_viz_fashion/RShingMap/server.R
+++ doc/flight_viz_fashion/RShingMap/server.R
@@ -15,30 +15,39 @@ shinyServer(function(input, output) {
   # Identify origin and destination
   flightLines <- reactive({
     
-    allPairs <- subset(flightData, FL_DATE == input$range )%>%
+    allPairs <- subset(flightData, FL_DATE == input$range)%>%
       select(ORIGIN, DEST)
     
+    allPairs$ORIGIN_Lon <- airportLocation[allPairs[,1],1]
+    allPairs$ORIGIN_Lat <- airportLocation[allPairs[,1],2]
+    allPairs$DEST_Lon <- airportLocation[allPairs[,2],1]
+    allPairs$DEST_Lat <- airportLocation[allPairs[,2],2]
+    allPairs <- na.omit(allPairs)
+    
     inters <- character(0)
-    for(i in 1:nrow(allPairs)){
-      inter <- gcIntermediate(airportLocation[allPairs[i,1],], 
-                              airportLocation[allPairs[i,2],],
-                              n=10, addStartEnd=TRUE, sp = TRUE, breakAtDateLine = TRUE)
-      
-      #lines(inter, col="slateblue", lwd=2)
-      inters <- c(inters, inter)
-    }
-    ll0 <- lapply( inters , function(x) `@`(x , "lines") )
-    ll1 <- lapply( unlist( ll0 ) , function(y) `@`(y,"Lines") )
-    SpatialLines( list( Lines( unlist( ll1 ) , ID = 1) ) )
+    
+    gcIntermediate(allPairs[,c("ORIGIN_Lon", "ORIGIN_Lat")], 
+                   allPairs[,c("DEST_Lon", "DEST_Lat")], 
+                    n=10, addStartEnd=TRUE, sp = TRUE, breakAtDateLine = TRUE)
+
+    #for(i in 1:nrow(allPairs)){
+    #  inter <- gcIntermediate(airportLocation[allPairs[i,1],], 
+    #                          airportLocation[allPairs[i,2],],
+    #                          n=10, addStartEnd=TRUE, sp = TRUE, breakAtDateLine = TRUE)
+    #  inters <- c(inters, inter)
+    #}
+    #ll0 <- lapply( inters , function(x) `@`(x , "lines") )
+    #ll1 <- lapply( unlist( ll0 ) , function(y) `@`(y,"Lines") )
+    #SpatialLines( list( Lines( unlist( ll1 ) , ID = 1) ) )
   })
   
-  
-  
-  
+
   
   # Output the route map  
   output$map <- renderLeaflet({
-    leaflet(flightLines()) %>% addTiles() %>% addPolylines()
+    leaflet(flightLines()) %>% 
+      addTiles() %>% 
+      addPolylines(color = 'red', weight=1.5)
   })  
 }
 )
