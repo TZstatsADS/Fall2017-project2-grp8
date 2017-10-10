b026ca3a630f107a378e777a3cbd47071d5faa29
duanshiqi <duanshiqi@users.noreply.github.com>
2017-10-09 20:49:42 -0400

    some local changes

--- app/server.R
+++ app/server.R
@@ -49,6 +49,15 @@ shinyServer(function(input, output) {
                                                                               origin=input$origin2,
                                                                               destination=input$destination2,
                                                                               month=input$month2))
+  output$treemap<-renderPlot({
+    #selcet a destination, an origin, a month and a satisfied time point
+    tree_select=tree_final1%>%
+      filter(dest==input$destination,orig==input$origin,month==input$mon,sat_time==input$satisfy_time)
+    if(nrow(tree_select)!=0){
+      tree_select$label<-paste(tree_select$carrier,", ",round(100*tree_select$prec,2),"%",sep="")
+      treemap(tree_select,index='label',vSize="prec",vColor="label",type="categorical", palette=rainbow(7),aspRatio=30/30,drop.unused.levels = FALSE, position.legend="none")
+    }
+  })
   
  
 })
